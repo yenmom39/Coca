b5cbe8f42c227b1b5aad563e6cd6379bf16fe387
yenmom39 <156192433+yenmom39@users.noreply.github.com>
2024-02-12 14:52:37 +0700

    Coca

---
+++ Demo.java
@@ -0,0 +1,18 @@
+package fistProgram;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//variable
+		//String name ;
+		//name ="Coca ";
+		String name="Coca";
+		
+		System.out.println(name);
+		
+		System.out.print("Hello , I am a new Programer. I am " + name +".");
+
+	}
+
+}
---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+package fistProgram;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World program");
+		
+		System.out.print("Testhing\n");
+		
+		System.out.print("\'Testhing1\' : \t \"for me\"");
+		
+	}
+
+}
